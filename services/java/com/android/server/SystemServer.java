ad819cb8a6bc979c53c09147b420d654b70dbe63
Chris Lahaye <dev@chrislahaye.com>
2017-04-25 00:10:35 +0200

    pocket: introduce pocket bridge
    This service communicates pocket state to the pocket judge kernel driver.
    It maintains the pocket state by binding to the pocket service.
    
    Ticket: NOUGAT-49
    
    Change-Id: Iee77f767f23d3f77a0d1d871f9305f3b3c6d6630
    Signed-off-by: Chris Lahaye <dev@chrislahaye.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -95,6 +95,7 @@ import com.android.server.om.OverlayManagerService;
 import com.android.server.os.DeviceIdentifiersPolicyService;
 import com.android.server.os.SchedulingPolicyService;
 import com.android.server.pocket.PocketService;
+import com.android.server.pocket.PocketBridgeService;
 import com.android.server.pm.BackgroundDexOptService;
 import com.android.server.pm.CrossProfileAppsService;
 import com.android.server.pm.Installer;
@@ -1574,6 +1575,13 @@ public final class SystemServer {
             mSystemServiceManager.startService(PocketService.class);
             traceEnd();
 
+            if (!context.getResources().getString(
+                    com.android.internal.R.string.config_pocketBridgeSysfsInpocket).isEmpty()) {
+                traceBeginAndSlog("StartPocketBridgeService");
+                mSystemServiceManager.startService(PocketBridgeService.class);
+                traceEnd();
+		}
+
             // LiveDisplay
             if (!mOnlyCore){
                 traceBeginAndSlog("StartLineageHardwareService");
@@ -1581,7 +1589,7 @@ public final class SystemServer {
                 traceEnd();
                 traceBeginAndSlog("StartLiveDisplayService");
                 mSystemServiceManager.startService(LiveDisplayService.class);
-                traceEnd();
+
             }
         }
 
