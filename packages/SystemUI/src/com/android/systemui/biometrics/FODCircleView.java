6d5520598adb75af5665526ba6c75f20178616c1
TheScarastic <warabhishek@gmail.com>
2019-09-10 11:07:39 +0530

    FODCircleView: Add support for custom FP pressed icon
    * For those devices which were calibrated to use
      non-solid color icons, add the icon in overlay.
    
    Change-Id: Ib8a19f5bf9aa9e18a6a9d8d1061aac0f984f5132

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -347,6 +347,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
                 super.onDraw(canvas);
             }
         };
+        mPressedView.setImageResource(R.drawable.fod_icon_pressed);
 
         mWindowManager.addView(this, mParams);
 
