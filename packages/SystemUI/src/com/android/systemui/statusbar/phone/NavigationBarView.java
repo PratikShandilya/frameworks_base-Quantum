LineageOS/android_frameworks_base

Prevent NFE in SystemUI when parsing invalid int
* Also for custom lineage keys

Change-Id: Ie0d63e2c3af2c613bb8c9bdf6ff97637e754b7ae

packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -1170,7 +1170,7 @@ public class NavigationBarView extends FrameLayout implements PluginListener<Nav
     @Override
     public void onTuningChanged(String key, String newValue) {
         if (NAVIGATION_BAR_MENU_ARROW_KEYS.equals(key)) {
-            mShowDpadArrowKeys = newValue != null && Integer.parseInt(newValue) != 0;
+            mShowDpadArrowKeys = TunerService.parseIntegerSwitch(newValue, false);
             setNavigationIconHints(mNavigationIconHints);
         }
     }

PratikShandilya/frameworks_base-Quantum

packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -1191,7 +1191,7 @@ public class NavigationBarView extends FrameLayout implements
     @Override
     public void onTuningChanged(String key, String newValue) {
         if (NAVIGATION_BAR_MENU_ARROW_KEYS.equals(key)) {
-            mShowCursorKeys = newValue != null && Integer.parseInt(newValue) != 0;
+            mShowCursorKeys = TunerService.parseIntegerSwitch(newValue, false);
             setNavigationIconHints(mNavigationIconHints);
         }
     }